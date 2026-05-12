47e835ff704041b0c1f26b6442d21dc93d25584a
MobileID <123370216+vbmobile@users.noreply.github.com>
2026-05-12 11:39:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "docscanner_ios",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/DocScannerIOS/DocScanner-4.1.12.xcframework.zip",
-            checksum: "39d83bb9c7ea06547116d0e43d0b6e8f32f03b03843a00ae69e548c59c2b4c37"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/DocScannerIOS/DocScanner-4.1.13.xcframework.zip",
+            checksum: "70dfab64908ba7f6363608a371947d8639f9f311204b54d6fb66e428ea74e8d1"
         ),
         .target(
             name: "AMADocScanneriOS",
