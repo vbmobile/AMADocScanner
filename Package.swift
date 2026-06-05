cdb806cb1aa10972af0a4ec18a94ed14ea580612
Tiago Silva <tiago.silva@vision-box.com>
2026-06-05 10:16:33 +0100

    TRAVELERID-14693: 4.1.13-hotfix Release

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "docscanner_ios",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/DocScannerIOS/DocScanner-4.1.13.xcframework.zip",
-            checksum: "70dfab64908ba7f6363608a371947d8639f9f311204b54d6fb66e428ea74e8d1"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/DocScannerIOS/DocScanner-4.1.13-hotfix.xcframework.zip",
+            checksum: "88fb64fc77e94676f483958cbcaf76bfac9f7be4e56edc5aeb332ca8225fa3df"
         ),
         .target(
             name: "AMADocScanneriOS",
